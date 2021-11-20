97ddb8bbde798d0bab7ab21cd083189eb47d97d5
Godswill Edet <gimmex1@gmail.com>
2021-11-20 18:12:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react"
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <h1>Reddit Clone</h1>
     </div>
   );
 }
